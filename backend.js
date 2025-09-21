ea191546d4a752ea769574540170ea87187328c7
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-09-21 11:23:45 -0300

    alterando estrutura dos blocos, apts e vagas

---
+++ backend.js
@@ -0,0 +1,27 @@
+var apartmentBlocks = {
+  A: [
+    {
+      numero_apto: 101,
+      vagas: [
+        { numero: 111, ocupada: false },
+        { numero: 222, ocupada: false },
+      ],
+    },
+    { numero_apto: 103, vagas: [{ numero: 333, ocupada: false }] },
+    { numero_apto: 104, vagas: [{ numero: 444, ocupada: false }] },
+  ],
+  B: [
+    { numero_apto: 404, vagas: [{ numero: 4, ocupada: false }] },
+    { numero_apto: 422, vagas: [{ numero: 2, ocupada: false }] },
+    { numero_apto: 409, vagas: [{ numero: 9, ocupada: false }] },
+  ],
+};
+
+/**
+ * Obtém apartamentos de um bloco
+ * @param {string} bloco - Letra do bloco (A, B, C...)
+ * @returns {Array<Object>} Lista de apartamentos
+ */
+function getApartmentsByBlock(block) {
+  return apartmentBlocks[block] || [];
+}
