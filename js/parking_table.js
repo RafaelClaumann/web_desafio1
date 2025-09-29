04f1d0606844d57d2fbcbf7a20cc50c25c1d1982
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-09-29 08:33:37 -0300

    criando highlight nas vagas ocupadas

--- js/parking_table.js
+++ js/parking_table.js
@@ -1,26 +1,51 @@
-var tbody = document.querySelector("#parking-table tbody");
+function fillParkingTableOnLoad() {
+  var tbody = document.querySelector("#parking-table tbody");
 
-const parkingSpots = getParkingSpotsWithBlockAndApartment();
-parkingSpots.forEach((parkingSpot) => {
-  const tableRow = document.createElement("tr");
+  const parkingSpots = getParkingSpotsWithBlockAndApartment();
+  parkingSpots.forEach((parkingSpot) => {
+    const tableRow = document.createElement("tr");
 
-  const thBloco = document.createElement("th");
-  thBloco.scope = "row";
-  thBloco.textContent = parkingSpot.bloco;
+    const thBloco = document.createElement("th");
+    thBloco.scope = "row";
+    thBloco.textContent = parkingSpot.bloco;
 
-  const tdApartamento = document.createElement("td");
-  tdApartamento.textContent = parkingSpot.apartamento;
+    const tdApartamento = document.createElement("td");
+    tdApartamento.textContent = parkingSpot.apartamento;
 
-  const tdVaga = document.createElement("td");
-  tdVaga.textContent = parkingSpot.numero;
+    const tdVaga = document.createElement("td");
+    tdVaga.textContent = parkingSpot.numero;
 
-  const tdPlaca = document.createElement("td");
-  tdPlaca.textContent = parkingSpot.vehicle?.plate || "vazia";
+    const tdPlaca = document.createElement("td");
+    tdPlaca.textContent = parkingSpot.vehicle?.plate || "vazia";
 
-  tableRow.appendChild(thBloco);
-  tableRow.appendChild(tdApartamento);
-  tableRow.appendChild(tdVaga);
-  tableRow.appendChild(tdPlaca);
+    tableRow.appendChild(thBloco);
+    tableRow.appendChild(tdApartamento);
+    tableRow.appendChild(tdVaga);
+    tableRow.appendChild(tdPlaca);
 
-  tbody.appendChild(tableRow);
+    tbody.appendChild(tableRow);
+  });
+}
+
+function highlightOccupiedParkingSpots() {
+  const tabela = document.getElementById("parking-table");
+  const linhas = tabela.getElementsByTagName("tr");
+
+  console.log(JSON.stringify(tabela));
+  console.log(JSON.stringify(linhas));
+
+  for (let i = 1; i < linhas.length; i++) {
+    const cells = linhas[i].getElementsByTagName("td");
+    const placa = cells[2]?.textContent.trim();
+
+    if ("vazia" != placa) {
+      linhas[i].classList.add("highlight");
+      console.log(`apto ${cells[0].textContent?.trim()} / vaga: ${cells[1].textContent?.trim()} / placa ${cells[2].textContent?.trim()}`);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fillParkingTableOnLoad();
+  highlightOccupiedParkingSpots();
 });
