751ce5bcb76b8bfac6814c7c0188663c65824682
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-09-28 23:51:03 -0300

    correçoes backend.js

--- js/backend.js
+++ js/backend.js
@@ -139,48 +139,6 @@ function getParkingSpot(block, apartmentNumber, spotNumber) {
 function getParkingSpotsWithBlockAndApartment() {
   const _apartmentBlocks = _getApartmentBlocks();
 
-  // permitir que o cliente cadastre a mesma placa nas vagas que lhe pertence
-  // nao permitir que cadastre o mesmo carro em mais de uma vaga
-
-  // BLOCO
-  // APARTAMENTO
-  // VAGA
-  // PLACA
-
-  let parkingSpots = Object.entries(_apartmentBlocks).flatMap(
-    ([block, apartments]) =>
-      apartments.flatMap((apartment) =>
-        apartment.vagas.map((vaga) => ({
-          bloco: block,
-          apartamento: apartment.numero_apto,
-          ...vaga,
-        }))
-      )
-  );
-
-  let block = "A";
-  let apartamento = 101;
-  let vaga = 222;
-  let placa = "MKU4J39";
-  parkingSpots.filter((spot) => {
-    // Encontramos uma vaga associada a placa
-
-    if (isPlateAlreadyPresent(placa)) {
-    }
-
-    if (placa == spot.vehicle?.plate) {
-      // O veiculo já está associado a outra vaga do mesmo apartamento
-      if (
-        vaga != spot.numero &&
-        apartamento == spot.apartamento &&
-        block == spot.bloco
-      ) {
-        return true;
-      }
-    }
-    return false;
-  });
-
   return Object.entries(_apartmentBlocks).flatMap(([block, apartments]) =>
     apartments.flatMap((apartment) =>
       apartment.vagas.map((vaga) => ({
